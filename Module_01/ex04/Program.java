6c8ecec22c1183624b900710c70e3d340ede41f8
laila-chk <chokri.laila.ch@gmail.com>
2024-04-04 12:34:01 +0000

    tst

---
+++ Module_01/ex04/Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.print("->");
+    String input = inp.nextLine();
+    System.out.print(input);
+    //analysing the input:
+  }
+}
