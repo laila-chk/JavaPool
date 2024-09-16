9931bd9461a0cacea34e1a38ecf9a7026ee4aebe
laila-chk <chokri.laila.ch@gmail.com>
2024-09-16 08:46:44 +0100

    modified 00-00 due to details i didn't notice at first

---
+++ Module_00/ex00/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+  public static void main(String[] args) {
+    int num = 479598;
+    int res = 0;
+    while (num != 0){
+      res += num % 10;
+      num /= 10;
+    }
+    System.out.println(res);
+  }
+}
